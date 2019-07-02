879d76959244052b0d5b5e12510b1c6e4671242d
Olushi Luqman <olushilukmon03@gmail.com>
2019-07-02 14:41:16 +0100

    fix: Unknown modifier:  error with schema

--- server/models/song.js
+++ server/models/song.js
@@ -1,34 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SongSchema = new Schema({
-  title: { type: String },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'user'
+const SongSchema = new Schema(
+  {
+    title: { type: String },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user"
+    },
+    lyrics: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "lyric"
+      }
+    ]
   },
-  lyrics: [{
-    type: Schema.Types.ObjectId,
-    ref: 'lyric'
-  }]
-});
+  {
+    usePushEach: true
+  }
+);
 
-SongSchema.statics.addLyric = function(id, content) {
-  const Lyric = mongoose.model('lyric');
+SongSchema.statics.addLyric = async function(id, content) {
+  const Lyric = mongoose.model("lyric");
 
-  return this.findById(id)
-    .then(song => {
-      const lyric = new Lyric({ content, song })
-      song.lyrics.push(lyric)
-      return Promise.all([lyric.save(), song.save()])
-        .then(([lyric, song]) => song);
-    });
-}
+  try {
+    const song = await this.findById(id);
+    const lyric = new Lyric({ content, song });
+    song.lyrics.push(lyric.id);
+    await lyric.save();
+    await song.save();
+
+    return song;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 SongSchema.statics.findLyrics = function(id) {
   return this.findById(id)
-    .populate('lyrics')
-    .then(song => song.lyrics);
-}
+    .populate("lyrics")
+    .then(song => {
+      return song.lyrics;
+    });
+};
 
-mongoose.model('song', SongSchema);
+mongoose.model("song", SongSchema);
